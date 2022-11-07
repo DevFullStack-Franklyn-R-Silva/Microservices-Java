7a8d3fa4d5fb19eedab9b3b4004802d88745b86d
Franklyn R <franklyn1998@live.com>
2022-11-07 13:10:09 -0300

    Os Verbos POST, PUT e DELETE

--- Rest/src/main/java/com/github/hadesfranklyn/rest/controller/PersonController.java
+++ Rest/src/main/java/com/github/hadesfranklyn/rest/controller/PersonController.java
@@ -1,8 +1,11 @@
 package com.github.hadesfranklyn.rest.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +18,38 @@ import com.github.hadesfranklyn.rest.service.PersonServices;
 public class PersonController {
 
 	@Autowired
-	private PersonServices services;
-
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person sum(@PathVariable("id") String id) throws Exception {
-
-		return services.findById(id);
+	private PersonServices service;
+	
+	@RequestMapping(method=RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findAll() {
+		return service.findAll();
+	}	
+	
+	@RequestMapping(value="/{id}",
+			method=RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person findById(@PathVariable("id") String id) {
+		return service.findById(id);
+	}	
+	
+	@RequestMapping(method=RequestMethod.POST,
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person create(@RequestBody Person person) {
+		return service.create(person);
 	}
-
+	
+	@RequestMapping(method=RequestMethod.PUT,
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person update(@RequestBody Person person) {
+		return service.update(person);
+	}	
+	
+	@RequestMapping(value="/{id}", 
+			method=RequestMethod.DELETE)
+	public void delete(@PathVariable("id") String id) {
+		service.delete(id);
+	}	
 }
--- Rest/src/main/java/com/github/hadesfranklyn/rest/service/PersonServices.java
+++ Rest/src/main/java/com/github/hadesfranklyn/rest/service/PersonServices.java
@@ -1,5 +1,7 @@
 package com.github.hadesfranklyn.rest.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.stereotype.Service;
@@ -8,16 +10,47 @@ import com.github.hadesfranklyn.rest.model.Person;
 
 @Service
 public class PersonServices {
-   
+
 	private final AtomicLong counter = new AtomicLong();
-	
+
+	public Person create(Person person) {
+		return person;
+	}
+
+	public Person update(Person person) {
+		return person;
+	}
+
+	public void delete(String id) {
+
+	}
+
 	public Person findById(String id) {
 		Person person = new Person();
 		person.setId(counter.incrementAndGet());
-		person.setFirstName("Franklyn");
-		person.setLastName("Roberto");
-		person.setAddress("Arapiraca - AL");
-		person.setGender("Masculino");
+		person.setFirstName("Leandro");
+		person.setLastName("Costa");
+		person.setAddress("Uberlândia - Minas Gerais -Brasil");
+		person.setGender("Male");
+		return person;
+	}
+
+	public List<Person> findAll() {
+		List<Person> persons = new ArrayList<Person>();
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		return persons;
+	}
+
+	private Person mockPerson(int i) {
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Person name" + i);
+		person.setLastName("Last name" + i);
+		person.setAddress("Some address in Brasil" + i);
+		person.setGender("Male");
 		return person;
 	}
 }
