f083b2783493de1b0f6af65ff3b593da879f5646
Franklyn R <franklyn1998@live.com>
2022-11-07 11:32:26 -0300

    Implementando o Controller @RestController, @RequestMapping e @PathVariable

---
+++ Rest/src/main/java/com/github/hadesfranklyn/rest/MathController.java
@@ -0,0 +1,40 @@
+package com.github.hadesfranklyn.rest;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MathController {
+
+	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+	public Double greeting(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo)
+			throws Exception {
+		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new Exception();
+		}
+		Double sum = convertToDouble(numberOne) + convertToDouble(numberTwo);
+
+		return sum;
+	}
+
+	private Double convertToDouble(String strNumber) {
+		if (strNumber == null)
+			return 0D;
+		String number = strNumber.replaceAll(",", ".");
+
+		if (isNumeric(number))
+			return Double.parseDouble(number);
+
+		return 1D;
+	}
+
+	private boolean isNumeric(String strNumber) {
+		if (strNumber == null)
+			return false;
+		String number = strNumber.replaceAll(",", ".");
+
+		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+	}
+}
